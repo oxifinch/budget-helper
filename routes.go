096c5fb02b25f945574f3c713ca545d02f90c77a
oxifinch <oxifinch@protonmail.com>
2022-04-11 09:17:51 +0200

    feat: serving static page with home route

---
+++ routes.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// Routes are named as "routeDOMAIN"
+
+func routeHome(writer http.ResponseWriter, req *http.Request) {
+	_, err := fmt.Fprintf(writer, "Welcome to Budget Helper!\n")
+	if err != nil {
+		panic(err)
+	}
+}
+
+// TODO: This method of registering routes works, but will probably not be nice
+// to work with when there are A LOT of routes which then need to be manually
+// added/removed from this function. Is there a better way?
+func initRouter() http.ServeMux {
+	router := http.NewServeMux()
+
+	router.HandleFunc("/", routeHome)
+
+	return *router
+}
