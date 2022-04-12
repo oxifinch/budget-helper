7196815f782a0ea78bf7e88062141771c4129021
oxifinch <oxifinch@protonmail.com>
2022-04-12 09:27:00 +0200

    fix: first code review

--- routes.go
+++ routes.go
@@ -2,25 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 // Routes are named as "routeDOMAIN"
 
-func routeHome(writer http.ResponseWriter, req *http.Request) {
-	_, err := fmt.Fprintf(writer, "Welcome to Budget Helper!\n")
+func routeHome(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "Welcome to Budget Helper!\n")
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 }
 
-// TODO: This method of registering routes works, but will probably not be nice
-// to work with when there are A LOT of routes which then need to be manually
-// added/removed from this function. Is there a better way?
-func initRouter() http.ServeMux {
+func initRouter() *http.ServeMux {
 	router := http.NewServeMux()
 
 	router.HandleFunc("/", routeHome)
 
-	return *router
+	return router
 }
