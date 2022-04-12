487d4a31bf8a1da3268480f931b357f070b83f9a
oxifinch <oxifinch@protonmail.com>
2022-04-12 10:39:02 +0200

    fix: cleaned up error handle on HTTP listener

--- main.go
+++ main.go
@@ -9,18 +9,11 @@ import (
 const port = ":8080"
 
 func main() {
-	// Initialization steps:
-	// - [x] Register routes in the http.ServeMux called "router"
-	// - [x] Connect to database
-	// - [x] Listen for connections on port
 	router := initRouter()
 
 	db := database.NewDatabase()
 	defer db.Close()
 
 	log.Printf("Server started on PORT %v...\n", port)
-	err := http.ListenAndServe(port, router)
-	if err != nil {
-		log.Fatalf("error: failed to start listener: %v", err)
-	}
+	log.Fatal(http.ListenAndServe(port, router))
 }
