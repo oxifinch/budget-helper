9df86f8a58ccc8410b90dd8a14ff7d7c425a7daa
oxifinch <oxifinch@protonmail.com>
2022-04-21 13:57:03 +0200

    fix: merged routes into single file

--- routes.go
+++ routes.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"budget-helper/database"
-	"budget-helper/routes"
+	"fmt"
+	"log"
 	"net/http"
 )
+
+func home(db *database.Database, w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprint(w, "Welcome to Budget Helper!\n")
+	if err != nil {
+		log.Panic(err)
+	}
+}
 
 func wrapWithDB(db *database.Database, f func(*database.Database, http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -18,7 +26,7 @@ func initRouter() *http.ServeMux {
 	router := http.NewServeMux()
 
 	// REGISTER ALL ROUTES HERE
-	router.HandleFunc("/", wrapWithDB(db, routes.Home))
+	router.HandleFunc("/", wrapWithDB(db, home))
 
 	defer db.Close()
 	return router
