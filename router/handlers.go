3150ca8216f145ff4d55fa7c85af59c1a7ee5f82
oxifinch <oxifinch@protonmail.com>
2022-04-28 13:58:58 +0200

    rebuilt templates

--- router/handlers.go
+++ router/handlers.go
@@ -7,12 +7,21 @@ import (
 )
 
 var (
-	tmplHome  = template.Must(template.ParseFiles("./templates/pages/home.html"))
-	tmplLogin = template.Must(template.ParseFiles("./templates/pages/login.html"))
+	// Home template is created here, and it has access to home.html and base.html => see /templates/base.html
+	tmplHome  = template.Must(template.ParseFiles("./templates/pages/home.html", "./templates/base.html"))
+	tmplLogin = template.Must(template.ParseFiles("./templates/pages/login.html", "./templates/base.html"))
 )
 
 func (rt *Router) handleHome(w http.ResponseWriter, r *http.Request) {
-	err := tmplHome.Execute(w, nil)
+	data := struct {
+		AppTitle  string
+		PageTitle string
+	}{
+		AppTitle:  "Budget Helper",
+		PageTitle: "Home",
+	}
+
+	err := tmplHome.Execute(w, data)
 	if err != nil {
 		log.Fatalf("handleHome: %v\n", err)
 	}
