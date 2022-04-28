70dc1b31024a5771581926b4f4530607422cf5c3
oxifinch <oxifinch@protonmail.com>
2022-04-28 14:01:28 +0200

    fixed broken template rendering

--- router/handlers.go
+++ router/handlers.go
@@ -7,7 +7,6 @@ import (
 )
 
 var (
-	// Home template is created here, and it has access to home.html and base.html => see /templates/base.html
 	tmplHome  = template.Must(template.ParseFiles("./templates/pages/home.html", "./templates/base.html"))
 	tmplLogin = template.Must(template.ParseFiles("./templates/pages/login.html", "./templates/base.html"))
 )
@@ -21,7 +20,7 @@ func (rt *Router) handleHome(w http.ResponseWriter, r *http.Request) {
 		PageTitle: "Home",
 	}
 
-	err := tmplHome.Execute(w, data)
+	err := tmplHome.ExecuteTemplate(w, "base", data)
 	if err != nil {
 		log.Fatalf("handleHome: %v\n", err)
 	}
