74bcc87e6c7723848832cd2123bab23ea0397414
oxifinch <oxifinch@protonmail.com>
2022-04-29 09:07:14 +0200

    changed new user to uppercase and added debug prints

--- users/repo.go
+++ users/repo.go
@@ -4,6 +4,7 @@ import (
 	"budget-helper/database"
 	"budget-helper/models"
 	"context"
+	"fmt"
 
 	"github.com/volatiletech/sqlboiler/v4/boil"
 	. "github.com/volatiletech/sqlboiler/v4/queries/qm"
@@ -27,11 +28,19 @@ func (u *UserRepo) GetAll() (models.UserSlice, error) {
 }
 
 func (u *UserRepo) Create(username string, password string) (int64, error) {
-	user := models.User{
+	Usr := models.User{
 		Username: username,
 		Password: password,
 	}
 
-	err := user.Insert(u.ctx, u.db, boil.Infer())
-	return user.UserID, err
+	// All fields have been set correctly and username and password are included.
+	// It seems that boil.Infer cannot find the right columns, but using Whitelist
+	// or Greylist doesn't work either. Is there something wrong with the schema?
+	fmt.Printf("Usr: %v\n", Usr)
+	fmt.Printf("UserId: %v\n", Usr.UserID)
+	fmt.Printf("Username: %v\n", Usr.Username)
+	fmt.Printf("Password: %v\n", Usr.Password)
+
+	err := Usr.Insert(u.ctx, u.db, boil.Infer())
+	return Usr.UserID, err
 }
