a465bbaf50dc12624b82bc96dcde33ff87bdb4e4
oxifinch <oxifinch@protonmail.com>
2022-05-02 11:10:43 +0200

    fix: temporarily switched over to raw queries for inserts

--- users/repo.go
+++ users/repo.go
@@ -4,9 +4,7 @@ import (
 	"budget-helper/database"
 	"budget-helper/models"
 	"context"
-	"fmt"
 
-	"github.com/volatiletech/sqlboiler/v4/boil"
 	. "github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
 
@@ -28,19 +26,24 @@ func (u *UserRepo) GetAll() (models.UserSlice, error) {
 }
 
 func (u *UserRepo) Create(username string, password string) (int64, error) {
-	Usr := models.User{
-		Username: username,
-		Password: password,
-	}
+	// TODO: Fix SQLBoiler errors! Use this method only temporarily.
+	queryStr := "INSERT INTO user (username, password) VALUES (?, ?)"
 
-	// All fields have been set correctly and username and password are included.
-	// It seems that boil.Infer cannot find the right columns, but using Whitelist
-	// or Greylist doesn't work either. Is there something wrong with the schema?
-	fmt.Printf("Usr: %v\n", Usr)
-	fmt.Printf("UserId: %v\n", Usr.UserID)
-	fmt.Printf("Username: %v\n", Usr.Username)
-	fmt.Printf("Password: %v\n", Usr.Password)
+	query, err := u.db.Prepare(queryStr)
+	defer query.Close()
 
-	err := Usr.Insert(u.ctx, u.db, boil.Infer())
-	return Usr.UserID, err
+	res, err := query.Exec(username, password)
+
+	newId, err := res.LastInsertId()
+
+	return newId, err
+
+	// newUser := models.User{
+	// 	Username: username,
+	// 	Password: password,
+	// }
+
+	// boil.DebugMode = true
+	// err := newUser.Insert(u.ctx, u.db, boil.Infer())
+	// return newUser.UserID, err
 }
