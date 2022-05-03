83e0988c4ac0fdf65e271ea355bfac868e14b690
oxifinch <oxifinch@protonmail.com>
2022-05-03 11:41:21 +0200

    fix: removed comment in main.go

--- main.go
+++ main.go
@@ -8,8 +8,6 @@ const port = ":8080"
 
 func main() {
 	db := database.NewDatabase()
-	// TODO: GORM doesn't appear to have any close method(something about connection pooling?). Should I simply remove this?
-	// defer db.Close()
 
 	server := NewServer(port, db)
 	server.Run()
