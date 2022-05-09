8c0c103ce873f4abb2f68771ce8426a0115138be
oxifinch <oxifinch@protonmail.com>
2022-05-09 13:24:18 +0200

    finished basic New Budget form

--- router/budget.go
+++ router/budget.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"budget-helper/database"
 	"log"
 	"net/http"
 )
@@ -9,15 +10,23 @@ func (rt *Router) handleNewBudget(w http.ResponseWriter, r *http.Request) {
 	// TODO: User should be verified before anything else happens.
 	// displayLoginRequired(w, r)
 
-	data := struct {
-		AppTitle  string
-		PageTitle string
-	}{
-		AppTitle:  AppTitle,
-		PageTitle: "New Budget",
+	// TODO: Get user ID above
+	categories, err := rt.CategoryRepo.GetAllWithUserID(1)
+	if err != nil {
+		log.Fatalf("handleNewBudget: %v\n", err)
 	}
 
-	err := tmplNewBudget.ExecuteTemplate(w, "base", data)
+	data := struct {
+		AppTitle   string
+		PageTitle  string
+		Categories []database.Category
+	}{
+		AppTitle:   AppTitle,
+		PageTitle:  "New Budget",
+		Categories: categories,
+	}
+
+	err = tmplNewBudget.ExecuteTemplate(w, "base", data)
 	if err != nil {
 		log.Fatalf("handleNewBudget: %v\n", err)
 	}
