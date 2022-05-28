62e9dd173ae1b1b60adab6185980c288d4873c40
oxifinch <oxifinch@protonmail.com>
2022-05-28 11:18:48 +0200

    added gorilla session CookieStore to router

--- server.go
+++ server.go
@@ -10,21 +10,32 @@ import (
 	"budget-helper/users"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/gorilla/sessions"
 )
 
 type Server struct {
 	Router *router.Router
 	Port   string
+	Store  *sessions.CookieStore
 }
 
 func NewServer(port string, db *database.Database) *Server {
+	sessionkey, set := os.LookupEnv("BUDGET_HELPER_SESSION_KEY")
+	if !set {
+		log.Fatalf("NewServer: BUDGET_HELPER_SESSION_KEY not set.\n")
+	}
+
+	st := sessions.NewCookieStore([]byte(sessionkey))
+
 	u := users.NewUserRepo(db)
 	b := budgets.NewBudgetRepo(db)
 	c := categories.NewCategoryRepo(db)
 	p := payments.NewPaymentRepo(db)
 	i := incomeexpenses.NewIncomeExpenseRepo(db)
 
-	router := router.NewRouter(u, b, c, p, i)
+	router := router.NewRouter(u, b, c, p, i, st)
 	router.RegisterRoutes()
 
 	return &Server{
